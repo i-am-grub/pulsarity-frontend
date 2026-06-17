31129c8580f60a7b58d52c589ef3d8c9fe6e95b5
Bryce Gruber <dev@brycegruber.com>
2026-06-16 19:32:23 -0500

    setup dev proxy

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,15 @@ export default defineConfig({
           exclude: Features.LightDark,
       },
   },
+  server: {
+    host: 'localhost', // Changes the host URL from localhost
+    port: 3000,                  // Changes the port number
+    strictPort: true,            // Avoids trying next available port if 3000 is taken
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Backend URL
+        changeOrigin: true,
+        secure: false,
+      }
+    }}
 })
